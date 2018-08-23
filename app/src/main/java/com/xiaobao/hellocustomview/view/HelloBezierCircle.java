d44bbbf0a284896e8610b8b27c5a7041a4d31ee6
Y <1713050991@qq.com>
2018-08-23 18:40:05 +0800

    un

--- app/src/main/java/com/xiaobao/hellocustomview/view/HelloBezierCircle.java
+++ app/src/main/java/com/xiaobao/hellocustomview/view/HelloBezierCircle.java
@@ -1,5 +1,6 @@
 package com.xiaobao.hellocustomview.view;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,12 +10,13 @@ import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 public class HelloBezierCircle extends View {
 
     private Paint mPaint;
     private int mWidth, mHeight;
-    private PointF p1, p2, p3, p4;
+    private PointF p1_1, p1_2, p2_1, p2_2, p3_1, p3_2, p4_1, p4_2;
     private final double C = 0.551915024494f;
 
     public HelloBezierCircle(Context context) {
@@ -38,27 +40,27 @@ public class HelloBezierCircle extends View {
         super.onDraw(canvas);
 
         Path path = new Path();
-        path.moveTo(mWidth/2,p1.y);
-        path.quadTo(p1.x,p1.y,p1.x,mHeight/2);
-        path.quadTo(p2.x,p2.y,mWidth/2,p2.y);
-        path.quadTo(p3.x,p3.y,p3.x,mHeight/2);
-        path.quadTo(p4.x,p4.y,mWidth/2,p1.y);
-        canvas.drawPath(path,mPaint);
+        path.moveTo(mWidth / 2, mHeight / 2 - mWidth / 4);
+        path.cubicTo(p1_1.x, p1_1.y, p1_2.x, p1_2.y, p1_2.x, mHeight / 2);
+        path.cubicTo(p2_1.x, p2_1.y, p2_2.x, p2_2.y, mWidth / 2, p2_2.y);
+        path.cubicTo(p3_1.x, p3_1.y, p3_2.x, p3_2.y, p3_2.x, mHeight / 2);
+        path.cubicTo(p4_1.x, p4_1.y, p4_2.x, p4_2.y, mWidth / 2, p4_2.y);
+        canvas.drawPath(path, mPaint);
 
-        mPaint.setColor(Color.BLACK);
+        canvas.drawPoint(p1_1.x, p1_1.y, mPaint);
+        canvas.drawPoint(p1_2.x, p1_2.y, mPaint);
+        canvas.drawPoint(p2_1.x, p2_1.y, mPaint);
+        canvas.drawPoint(p2_2.x, p2_2.y, mPaint);
+        canvas.drawPoint(p3_1.x, p3_1.y, mPaint);
+        canvas.drawPoint(p3_2.x, p3_2.y, mPaint);
+        canvas.drawPoint(p4_1.x, p4_1.y, mPaint);
+        canvas.drawPoint(p4_2.x, p4_2.y, mPaint);
+
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStrokeWidth(2);
+
         canvas.drawLine(0, mHeight / 2, mWidth, mHeight / 2, mPaint);
         canvas.drawLine(mWidth / 2, 0, mWidth / 2, mHeight, mPaint);
-
-        mPaint.setColor(Color.RED);
-        mPaint.setStrokeWidth(2);
-        canvas.drawPoint(p1.x, p1.y, mPaint);
-        canvas.drawPoint(p2.x, p2.y, mPaint);
-        canvas.drawPoint(p3.x, p3.y, mPaint);
-        canvas.drawPoint(p4.x, p4.y, mPaint);
-//        canvas.drawLine(p1.x,p1.y,p2.x,p2.y,mPaint);
-//        canvas.drawLine(p2.x,p2.y,p3.x,p3.y,mPaint);
-//        canvas.drawLine(p3.x,p3.y,p4.x,p4.y,mPaint);
-//        canvas.drawLine(p4.x,p4.y,p1.x,p1.y,mPaint);
 
     }
 
@@ -69,9 +71,34 @@ public class HelloBezierCircle extends View {
         mPaint.setStrokeWidth(5);
         mPaint.setStyle(Paint.Style.STROKE);
 
-        p1 = new PointF(mWidth / 4 * 3, mHeight / 2 - mWidth / 4);
-        p2 = new PointF(mWidth / 4 * 3, mHeight / 2 + mWidth / 4);
-        p3 = new PointF(mWidth / 4, mHeight / 2 + mWidth / 4);
-        p4 = new PointF(mWidth / 4, mHeight / 2 - mWidth / 4);
+        p1_1 = new PointF((float) (mWidth / 2 + mWidth / 4 * C), mHeight / 2 - mWidth / 4);
+        p1_2 = new PointF(mWidth / 4 * 3, (float) (mHeight / 2 - mWidth / 4 * C));
+
+        p2_1 = new PointF(mWidth / 4 * 3, (float) (mHeight / 2 + mWidth / 4 * C));
+        p2_2 = new PointF((float) (mWidth / 2 + mWidth / 4 * C), mHeight / 2 + mWidth / 4);
+
+        p3_1 = new PointF((float) (mWidth / 2 - mWidth / 4 * C), mHeight / 2 + mWidth / 4);
+        p3_2 = new PointF(mWidth / 4, (float) (mHeight / 2 + mWidth / 4 * C));
+
+        p4_1 = new PointF(mWidth / 4, (float) (mHeight / 2 - mWidth / 4 * C));
+        p4_2 = new PointF((float) (mWidth / 2 - mWidth / 4 * C), mHeight / 2 - mWidth / 4);
+
+        start();
+    }
+
+    private void start() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(p1_2.x, mWidth);
+        valueAnimator.setDuration(10000);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                p1_2.x = p1_2.x + ((float) animation.getAnimatedValue());
+                p2_1.x = p2_1.x + ((float) animation.getAnimatedValue());
+                postInvalidate();
+            }
+        });
+        valueAnimator.start();
     }
 }
